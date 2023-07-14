5a69f40f67b4395a99ab254e3dc2e244718b7136
Nirvana Belen <nivaniz2019@gmail.com>
2023-07-14 16:56:45 -0600

    Update settings.py

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -19,12 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hln83r2jca&v#j^s%!$bsfypcn@c=zs77hzual4ib0!d2vf=z#'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = []
 
 
